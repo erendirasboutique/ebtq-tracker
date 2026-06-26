e5d383d53b3c58e0b9fb307b50509624ec6d33cc
Dylan <hello@erendirasboutique.com>
2026-06-25 18:22:05 -0700

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Erendira&apos;s Boutique Tracker</h1>
-      <p>The starter site is working.</p>
-    </main>
-  );
-}
